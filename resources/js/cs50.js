e71386b1ee6dc5630de233c462fe2072775f5c26
James Mete <jmete3@gmail.com>
2017-08-09 12:27:30 +0300

    updated legacy code

---
+++ resources/js/cs50.js
@@ -0,0 +1,68 @@
+
+// Function heavily inspired by online twitter api tutorials. 
+function listTweets(data) {
+	console.log(data);
+	var output = '<ul data-role="listview" data-theme="a">';
+	
+	$.each(data, function(key, val) {
+		var text = data[key].text;
+		var thumbnail = data[key].user.profile_image_url;
+		var name = data[key].user.name;
+		
+		text=text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(i) {
+			var url=i.link(i);
+			return url;
+		});
+		
+		text=text.replace(/[@]+[A-Za-z0-9-_]+/g, function(i) {
+			var item = i.replace("@",'');
+			var	url = i.link("http://twitter.com/"+ item);
+			return url;
+		});
+		
+		text=text.replace(/[#]+[A-Za-z0-9-_]+/g, function(i) {
+			var item = i.replace("#", '%23');
+			var url = i.link("http://search.twitter.com/search?q="+item);
+			return url;
+		});
+		
+		output += '<li>';
+		output += '<img src="' + thumbnail +'" alt="Photo of ' + name + '">';
+		output += '<div>' + text + '</div>';
+		output += '</li>';		
+	}); //go through each tweet
+	output += '</ul>';
+	$('#tweetlist').html(output);
+}
+
+
+// Reloads the function to get the tweets. It uses the text input to get the username.
+function reloadtwit(){
+    // Stores the text input in a variable
+    var sftweet = document.getElementById("tweetinput").value;
+    
+    if (sftweet == ""){
+        //Do nothing if textinput is empty. I could also set this to rever to CS50 timeline but that is a design debate.
+    }else{
+    // Attaches variable to full string needed to gather tweets from the twitter api.    
+    var fullsftweet = "tweets_json.php?screen_name="+sftweet+"&count=50";
+    //Gets the tweets
+    $.getJSON(fullsftweet,function(data) {
+    			listTweets(data);
+				$('#tweetlist').trigger('create');
+    
+		});
+        
+}
+}
+
+
+function redirect50(){
+    window.location.href = "http://jamesmete.com/TWEET50/";
+}
+
+
+
+
+
+
--- tweets_json.php
+++ tweets_json.php
@@ -1,8 +1,11 @@
 <?php
 require 'tmhOAuth.php'; // Get it from: https://github.com/themattharris/tmhOAuth
+// Above is a library used to access OAuth which is required by many api's in an easier way.
 
 // Use the data from http://dev.twitter.com/apps to fill out this info
 // notice the slight name difference in the last two items)
+
+// This information should be kept secret and security can be an issue.
 
 $connection = new tmhOAuth(array(
   'consumer_key' => '',
@@ -11,34 +14,36 @@ $connection = new tmhOAuth(array(
 	'user_secret' => '' //access token secret
 ));
 
-// set up parameters to pass
+// Sets up parameters to pass. These can be found in the twitter api documentation.
 $parameters = array();
 
+// Count parameter
 if ($_GET['count']) {
 	$parameters['count'] = strip_tags($_GET['count']);
 }
-
+// Screen name parameter. This is effectively usernames.
 if ($_GET['screen_name']) {
 	$parameters['screen_name'] = strip_tags($_GET['screen_name']);
 }
-
+// Twitter path parameter
 if ($_GET['twitter_path']) { $twitter_path = $_GET['twitter_path']; }  else {
 	$twitter_path = '1.1/statuses/user_timeline.json';
 }
 
 $http_code = $connection->request('GET', $connection->url($twitter_path), $parameters );
 
-if ($http_code === 200) { // if everything's good
+if ($http_code === 200) { // if successful
 	$response = strip_tags($connection->response['response']);
 
-	if ($_GET['callback']) { // if we ask for a jsonp callback function
+	if ($_GET['callback']) { // gets a response if we call for a jsonp callback function
 		echo $_GET['callback'],'(', $response,');';
 	} else {
 		echo $response;
 	}
 } else {
-	echo "Error ID: ",$http_code, "<br>\n";
+	echo "Error ID: ",$http_code, "<br>\n"; // Error debugging
 	echo "Error: ",$connection->response['error'], "<br>\n";
 }
 
-// You may have to download and copy http://curl.haxx.se/ca/cacert.pem
+// You may have to download and copy http://curl.haxx.se/ca/cacert.pem authentication certificate.
+// Might also require sudo curl if trying to recreate this on another server.
